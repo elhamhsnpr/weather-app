deed00e070134f00e06f9d77564775ccae05b36b
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-05-24 18:33:02 +0200

    refactor the code to use destruction and shorthand property

--- utils/forecast.js
+++ utils/forecast.js
@@ -6,13 +6,13 @@ const forecast = (latitude, longitude, callback) => {
     const APIkey = process.env.WEATHER_STACK_ACCESS_KEY
     const url = 'http://api.weatherstack.com/current?access_key=' + APIkey + '&query=' + latitude + ',' + longitude + '&units=f'
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, {body}) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out."
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out."
             )
         }
     })
--- utils/geocode.js
+++ utils/geocode.js
@@ -5,16 +5,16 @@ const geocode = (address, callback) => {
     const accessToken = process.env.GEO_CODE_ACCESS_TOKEN
     const url = 'https://api.mapbox.com/search/geocode/v6/forward?q=' + address + '&access_token=' + accessToken + '&limit=1'
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
-        } else if (response.body.features.length === 0) {
+        } else if (body.features.length === 0) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
             callback(undefined, {
-                longitude: response.body.features[0].properties.coordinates.longitude,
-                latitude: response.body.features[0].properties.coordinates.latitude,
-                location: response.body.features[0].properties.full_address
+                longitude: body.features[0].properties.coordinates.longitude,
+                latitude: body.features[0].properties.coordinates.latitude,
+                location: body.features[0].properties.full_address
 
             })
         }
