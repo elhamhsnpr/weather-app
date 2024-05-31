042ba9c6acb740107edb1c1fed55bc12ba81c30d
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-05-31 16:36:55 +0200

    created search form to get location and print it in client terminal

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+console.log('Cleint side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    console.log(location)
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.forecast)
+                console.log(data.location)
+            }
+
+        })
+    })
+
+
+})
