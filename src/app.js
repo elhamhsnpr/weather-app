aca0c3f1c55fc364d2ee3daee0b85cd1e3af7c3b
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-05-31 10:56:40 +0200

    add css stylesheet and add partials header and footer

--- src/app.js
+++ src/app.js
@@ -2,16 +2,19 @@ const express = require('express')
 const path = require('path')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+const hbs = require('hbs')
 
 const app = express()
 
 //Define path for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 //Setup handelebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 //Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
