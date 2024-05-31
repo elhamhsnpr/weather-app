8b54f19ac287829d5bd4adce66c947fbf81b4e76
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-05-31 13:34:49 +0200

    set geocode and forecast functions at /weather route

--- src/app.js
+++ src/app.js
@@ -28,16 +28,34 @@ app.get('', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
-    if (!req.query.address) {
+    const address = req.query.address
+
+    if (!address) {
         return res.send({
             error: 'You must provide a address term'
         })
     }
 
-    res.send({
-        forecast: 'it is sunny',
-        location: req.query.address
+    geocode(address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location: location,
+                address: address
+            })
+        })
+
     })
+
 })
 
 app.get('/help', (req, res) => {
@@ -76,28 +94,6 @@ app.listen(3000, () => {
     console.log('Server is up on port 3000')
 })
 
-
-
-// let address = process.argv[2]
-// if (!address) {
-//     console.log('Please provide an address!')
-// } else {
-//     geocode(address, (error, { latitude, longitude, location } = {}) => {
-//         if (error) {
-//             return console.log(error)
-//         }
-
-//         forecast(latitude, longitude, (error, forecastData) => {
-
-//             if (error) {
-//                 return console.log(error)
-//             }
-//             console.log(location)
-//             console.log(forecastData)
-//         })
-
-//     })
-// }
 
 
 
