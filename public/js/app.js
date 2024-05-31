c2b813df2ab6c04d3dedadb15b248629afcc8e1a
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-05-31 16:57:46 +0200

    display the location, forecast and error message on client side

--- public/js/app.js
+++ public/js/app.js
@@ -2,9 +2,14 @@ console.log('Cleint side javascript file is loaded!')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
 
     const location = search.value
     console.log(location)
@@ -13,9 +18,12 @@ weatherForm.addEventListener('submit', (e) => {
         response.json().then((data) => {
             if (data.error) {
                 console.log(data.error)
+                messageOne.textContent = data.error
             } else {
                 console.log(data.forecast)
                 console.log(data.location)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
             }
 
         })
