153ec3a362b05b7e25d103fa2f3d17c632e47e7d
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-06-03 17:57:54 +0200

    Add feelslike and humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,16 +4,16 @@ require('dotenv').config();
 const forecast = (latitude, longitude, callback) => {
 
     const APIkey = process.env.WEATHER_STACK_ACCESS_KEY
-    const url = 'http://api.weatherstack.com/current?access_key=' + APIkey + '&query=' + latitude + ',' + longitude + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=' + APIkey + '&query=' + latitude + ',' + longitude + '&units=m'
 
-    request({ url, json: true }, (error, {body}) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
             callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out."
-            )
+                + " It feels like " + body.current.feelslike + " degress out. The humidity is " + body.current.humidity + "%.")
         }
     })
 }
